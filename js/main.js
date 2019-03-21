050a55dc6b13177a7904e5413a4b817c17bd6940
MYBBSTYLE <MYBBSTYLE@users.noreply.github.com>
2019-03-21 22:23:32 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,76 @@
-$(document).ready(function(){
+$(function () {
+
     var mouseX, mouseY;
-    var ww = $( window ).width();
-    var wh = $( window ).height();
+    var ww = $(window).width();
+    var wh = $(window).height();
     var traX, traY;
-    $(document).mousemove(function(e){
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-      traX = ((4 * mouseX) / 570) + 40;
-      traY = ((4 * mouseY) / 570) + 50;
-      console.log(traX);
-      $(".title").css({"background-position": traX + "%" + traY + "%"});
+
+    $.scrollify({
+        section: ".section",
+        sectionName: "section-name",
+        interstitialSection: "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll: true,
+        before: function () { },
+        after: function () { },
+        afterResize: function () { },
+        afterRender: function () { }
     });
-  });
+
+    // $(".loader").delay(2000).fadeOut("slow");
+    // $("#overlayer").delay(2000).fadeOut("fast");
+  
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+        traX = ((4 * mouseX) / 570) + 40;
+        traY = ((4 * mouseY) / 570) + 50;
+        $(".title").css({ "background-position": traX + "%" + traY + "%" });
+    });
+
+
+    $('.toggle-nav').on('click', function () {
+        toggleNavigation($(this), $('.nav-pane'));
+
+    });
+    $("a").on("click", function () {
+        toggleNavigation($(".toggle-nav"), $(".nav-pane"));
+    });
+
+    function toggleNavigation(btn, nav) {
+
+        btn.toggleClass('open');
+        nav.toggleClass('open');
+        changeLetters($('.toggle-nav'));
+    }
+
+    function changeLetters(btn) {
+        let m = $('span.m');
+        let e = $('span.e');
+        let n = $('span.n');
+        let u = $('span.u');
+        // e.toggleClass('btn-close');
+
+        if (btn.hasClass('open')) {
+
+            m.text("E");
+            n.text("I");
+            e.addClass('btn-close');
+            u.text("T");
+        } else {
+            m.text("M");
+            e.removeClass('btn-close');
+            n.text("N");
+            u.text("U");
+
+        }
+    }
+})
